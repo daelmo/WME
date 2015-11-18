5385c1f72e0c133ffb2946e594e2c00bbfe1f753
Josephine Rehak <justallphine@googlemail.com>
2015-11-18 21:44:33 +0100

    save in variable

--- A3_Team9/app.js
+++ A3_Team9/app.js
@@ -1,11 +1,11 @@
-// // DO NOT CHANGE!
+// DO NOT CHANGE!
 //init app with express, util, body-parser, csv2json
 var express = require('express');
 var app = express();
 var sys = require('util');
 var path = require('path');
 var bodyParser = require('body-parser');
-var Converter = require("csv2json").Converter;
+var Converter = require("csvtojson").Converter;
 
 //register body-parser to handle json from res / req
 app.use( bodyParser.json() );
@@ -16,20 +16,23 @@ app.use( express.static( path.join(__dirname, "public") ) );
 // END DO NOT CHANGE!
 
 
-
 /**************************************************************************
 ****************************** csv2json *********************************
 **************************************************************************/
+// see http://www.scriptscoop.net/t/b682f7874bdf/javascript-how-to-convert-csv-to-json-in-node-js.html
+// by Keyang
+var json;
+//Converter Class
+var Converter=require("csvtojson").Converter;
+var writeStream = require("fs").createWriteStream("data.json");
+var fs=require("fs"); 
+var csvConverter=new Converter();
 
-var csv2json = require('csv2json');
-var fs = require('fs');
- 
-fs.createReadStream('world_data.csv')
-  .pipe(csv2json({
-    // Defaults to comma. 
-    separator: ','
-  }))
-  .pipe(fs.createWriteStream('data.json'));
+csvConverter.on("end_parsed",function(jsonObj){
+    json = jsonObj; //result json object
+});
+
+fs.createReadStream("./world_data.csv").pipe(csvConverter).pipe(writeStream);
 
 /**************************************************************************
 ********************** handle HTTP METHODS ***********************
@@ -38,7 +41,7 @@ fs.createReadStream('world_data.csv')
 
 // DO NOT CHANGE!
 // bind server to port
-var server = app.listen(3000, function () {
+var server = app.listen(8000, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
